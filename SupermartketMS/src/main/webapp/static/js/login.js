50ace505ea1e91add7eb7a97b2d8aa7dfc650c3c
Shizhong-Shang <ShizhongShang@cmail.carleton.ca>
2022-06-23 08:28:24 -0400

    .

---
+++ SupermartketMS/src/main/webapp/static/js/login.js
@@ -0,0 +1,29 @@
+/**
+ * Copyright (c) 2008-2024: Shizhong Shang
+ * Project: SummerRaid
+ * @className: test
+ * @Description: TODO
+ *
+ * @author: SHIZHONG SHANG
+ * @date: 2022/5/19 11:00
+ *
+ */
+
+//获取元素样式
+const signInBtn = document.getElementById("signIn");
+const signUpBtn = document.getElementById("signUp");
+const fistForm = document.getElementById("form1");
+const secondForm = document.getElementById("form2");
+const container = document.querySelector(".container");
+
+//移除signInBtn事件句柄
+signInBtn.addEventListener("click", () => {
+    container.classList.remove("right-panel-active");
+});
+//向signUpBtn添加事件句柄
+signUpBtn.addEventListener("click", () => {
+    container.classList.add("right-panel-active");
+});
+
+fistForm.addEventListener("submit", (e) => e.preventDefault());
+secondForm.addEventListener("submit", (e) => e.preventDefault());
