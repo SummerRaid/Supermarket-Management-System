7533f638faed58639571798c8a727738c7b5aa92
Shizhong-Shang <ShizhongShang@cmail.carleton.ca>
2022-06-30 15:19:31 -0400

    user form

--- SupermartketMS/src/main/webapp/static/js/role.js
+++ SupermartketMS/src/main/webapp/static/js/role.js
@@ -66,7 +66,6 @@ window.onload=function(){
                         operate:"addRole",
                     }
                 }).then(function(response){
-                    vue.getRoles();
                 }).catch(function(error){
 
                 });
@@ -77,56 +76,20 @@ window.onload=function(){
         }
     });
 };
-//<![CDATA[
-// 获取弹窗
-var modal = document.getElementById('myModal');
-
-// 打开弹窗的按钮对象
-var btn = document.getElementById("addBtn");
-
-// 获取 <span> 元素，用于关闭弹窗
-var span = document.querySelector('.close');
-var w1 = document.getElementById("w1");
-var w2 = document.getElementById("w2");
-var modal = document.getElementById("myModal");
-var main = $("body *:not(.modal, .modal-content, .modal-content>object, .model-content>span)");
-
-// 点击按钮打开弹窗
-btn.onclick = function() {
-    let hl = w1.children[0];
-    let hr = w1.children[1];
-    let cs = hr.children[0];
-    let ab = hr.children[1];
-    let button = ab.children[0];
-    modal.style.display = "block";
-    /*main.css("filter","blur(5px)");
-    $(".close").css("color", "#aaa");*/
-    w2.style.filter = "blur(5px)";
-    hl.style.filter = "blur(5px)";
-    cs.style.filter = "blur(5px)";
-    button.style.filter = "blur(5px)";
-}
-
-// 点击 <span> (x), 关闭弹窗
-span.onclick = function() {
-    modal.style.display = "none";
-    let hl = w1.children[0];
-    let hr = w1.children[1];
-    let cs = hr.children[0];
-    let ab = hr.children[1];
-    let button = ab.children[0];
-    /*main.css("filter","blur(5px)");
-    $(".close").css("color", "#aaa");*/
-    w2.style.filter = "none";
-    hl.style.filter = "none";
-    cs.style.filter = "none";
-    button.style.filter = "none";
-}
-
-// 在用户点击其他地方时，关闭弹窗
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+function check(){
+    if (checkEmpty(name)==true){
+        //vue.addNewRole();
     }
 }
-//]]>
+function checkEmpty(name) {
+    var text = $(name).html();
+    alert(text);
+    if (text.value === "") {
+        alert("不能为空哦！");
+        return false;
+    } else {
+        //vue.addNewRole();
+        alert("");
+        return true;
+    }
+}
--- SupermartketMS/src/main/webapp/static/js/user.js
+++ SupermartketMS/src/main/webapp/static/js/user.js
@@ -10,7 +10,7 @@
  */
 window.onload=function(){
     let vue = new Vue({
-        el:"#w2",
+        el: "#w2",
         data:{
             users:{},
             roles:{}
@@ -26,7 +26,6 @@ window.onload=function(){
                 }).then(function(response){
                     let user = response.data;
                     vue.users = user;
-                    console.log(vue.users);
                 }).catch(function(error){
 
                 });
@@ -82,7 +81,6 @@ window.onload=function(){
                         operate:"register",
                     }
                 }).then(function(response){
-                    vue.getUsers();
                 }).catch(function(error){
 
                 });
@@ -93,67 +91,40 @@ window.onload=function(){
             this.getRoles();
         }
     });
+    let myModal = new Vue({
+        el: "#myModal",
+        data:{
+            uName:"",
+            uPwd:"",
+            uTName:"",
+            uRole:"",
+            uPhone:"",
+            uAddress:"",
+        },
+        method:{
+            checkEmpty: function(){
+                let count = 0;
+                for(let i=0; i<data.size();i++){
+                    if(data[i]===""){
+                        alert(i+"不能为空");
+                        count++;
+                    }
+                }
+                if(count!==0){
+                    vue.addNewUser();
+                    alert("新用户添加成功")
+                }
+            }
+        }
+    })
 }
-function checkEmpty(name){
-    var text=$(name).html();
-    alert(text);
-    if(text.value===""){
-        alert("不能为空哦！");
-    }else{
-        vue.addNewUser();
-        alert("新用户添加成功！");
-    }
-}
-//<![CDATA[
-// 获取弹窗
-var modal = document.getElementById('myModal');
-
-// 打开弹窗的按钮对象
-var btn = document.getElementById("addBtn");
-
-// 获取 <span> 元素，用于关闭弹窗
-var span = document.querySelector('.close');
-var w1 = document.getElementById("w1");
-var w2 = document.getElementById("w2");
-var modal = document.getElementById("myModal");
-var main = $("body *:not(.modal, .modal-content, .modal-content>object, .model-content>span)");
-
-// 点击按钮打开弹窗
-btn.onclick = function() {
-    let hl = w1.children[0];
-    let hr = w1.children[1];
-    let cs = hr.children[0];
-    let ab = hr.children[1];
-    let button = ab.children[0];
-    modal.style.display = "block";
-    /*main.css("filter","blur(5px)");
-    $(".close").css("color", "#aaa");*/
-    w2.style.filter = "blur(5px)";
-    hl.style.filter = "blur(5px)";
-    cs.style.filter = "blur(5px)";
-    button.style.filter = "blur(5px)";
-}
-
-// 点击 <span> (x), 关闭弹窗
-span.onclick = function() {
-    modal.style.display = "none";
-    let hl = w1.children[0];
-    let hr = w1.children[1];
-    let cs = hr.children[0];
-    let ab = hr.children[1];
-    let button = ab.children[0];
-    /*main.css("filter","blur(5px)");
-    $(".close").css("color", "#aaa");*/
-    w2.style.filter = "none";
-    hl.style.filter = "none";
-    cs.style.filter = "none";
-    button.style.filter = "none";
-}
-
-// 在用户点击其他地方时，关闭弹窗
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-//]]>
+// function checkEmpty(name){
+//     var text=$(name).html();
+//     alert(text);
+//     if(text.value===""){
+//         alert("不能为空哦！");
+//     }else{
+//         vue.addNewUser();
+//         alert("新用户添加成功！");
+//     }
+// }
